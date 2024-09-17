1ebc655e22192ea700888fd03146897224eb96f4
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-09-17 14:27:29 -0300

    Feat: Adding links to the tech grid

--- src/components/HomepageTech.tsx
+++ src/components/HomepageTech.tsx
@@ -5,26 +5,38 @@ function HomepageTech() {
     <div className={styles["homepage-tech"]}>
       <div className={styles["tech-grid"]}>
         <section className={styles["tech-row"]}>
-          <div className={styles["tech-item"]}>
-            <img src="djangoLogo.png" alt="django-logo" />
-          </div>
-          <div className={styles["tech-item"]}>
-            <img src="reactLogo.svg" alt="react-logo" />
-          </div>
-          <div className={styles["tech-item"]}>
-            <img src="nodejsLogo.png" alt="node-logo" />
-          </div>
+          <a href="https://www.djangoproject.com/" target="_blank">
+            <div className={styles["tech-item"]}>
+              <img src="djangoLogo.png" alt="django-logo" />
+            </div>
+          </a>
+          <a href="https://reactjs.org/" target="_blank">
+            <div className={styles["tech-item"]}>
+              <img src="reactLogo.svg" alt="react-logo" />
+            </div>
+          </a>
+          <a href="https://nodejs.org/en/" target="_blank">
+            <div className={styles["tech-item"]}>
+              <img src="nodejsLogo.png" alt="node-logo" />
+            </div>
+          </a>
         </section>
         <section className={styles["tech-row"]}>
-          <div className={styles["tech-item"]}>
-            <img src="pythonLogo.png" alt="python-logo" />
-          </div>
-          <div className={styles["tech-item"]}>
-            <img src="mysqlLogo.png" alt="mysql-logo" />
-          </div>
-          <div className={styles["tech-item"]}>
-            <img src="typescriptLogo.png" alt="typescript-logo" />
-          </div>
+          <a href="https://www.python.org/" target="_blank">
+            <div className={styles["tech-item"]}>
+              <img src="pythonLogo.png" alt="python-logo" />
+            </div>
+          </a>
+          <a href="https://www.mysql.com/" target="_blank">
+            <div className={styles["tech-item"]}>
+              <img src="mysqlLogo.png" alt="mysql-logo" />
+            </div>
+          </a>
+          <a href="https://www.typescriptlang.org/" target="_blank">
+            <div className={styles["tech-item"]}>
+              <img src="typescriptLogo.png" alt="typescript-logo" />
+            </div>
+          </a>
         </section>
       </div>
     </div>
